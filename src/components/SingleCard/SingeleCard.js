41c77258febf8cbe86d3a36e5c1b5bb2601dc9bd
cepuii <54589413+cepuii@users.noreply.github.com>
2023-11-16 23:26:18 +0200

    Add singleCard component

--- src/components/SingleCard/SingeleCard.js
+++ src/components/SingleCard/SingeleCard.js
@@ -1,36 +1,40 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
 
-export default function SingleCard() {
-
-    
-
+export default function SingleCard({ id, name, time, image, handleId }) {
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component="img"
-        alt="green iguana"
-        height="140"
-        image="/static/images/cards/contemplative-reptile.jpg"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
+    <Card sx={{ width: "395px", height: "222px", position: "relative" }}>
+      <CardMedia component="img" alt={name} image={image} width={'100%'} height={'100%'} />
+      <div
+        style={{
+          position: "absolute",
+          width: "100%",
+          height: "100%",
+          paddingTop: "50px",
+          paddingLeft: "50px",
+          top: 0,
+          left: 0,
+          color: "white",
+          background:
+            "linear-gradient(90deg, rgba(0, 0, 0, 0.80) 0%, rgba(20, 20, 20, 0.40) 50%, rgba(83, 100, 141, 0.00) 100%",
+        }}
+      >
+        <p>{name}</p>
+        <p>{time}</p>
+        <button onClick={()=>handleId(id)}
+          style={{
+            border: "1px solid #000000",
+            borderRadius: "3px",
+            background: "#fca311",
+            width: "100px",
+            height: "30px",
+            color: "#000000",
+          }}
+        >
+          Show more
+        </button>
+      </div>
     </Card>
   );
 }
