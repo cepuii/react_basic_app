368b6ea8b2563c4b13a2ee73bb06f1c02e92536c
cepuii <54589413+cepuii@users.noreply.github.com>
2023-11-16 21:59:15 +0200

    add mui ImgMediaCard

--- src/App.js
+++ src/App.js
@@ -8,19 +8,7 @@ function App() {
   return (
     <div className="App">
       <Navigation pages={pages}></Navigation>
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
