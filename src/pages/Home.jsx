39fcc6d7d913f41feedd094011f152e9995fb019
cepuii <54589413+cepuii@users.noreply.github.com>
2023-12-16 18:19:45 +0200

    Delete search input from home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,67 +1,39 @@
 import useRequest from "../hooks/useRequest";
 import { Grid } from "@mui/material";
-import { useDispatch, useSelector } from "react-redux";
-import { setSearch } from "../store/SearchSlice";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Box } from "@mui/system";
+import TitleSingleSlide from "../components/TitleSingleSlide/TitleSingleSlide";
+import { Autoplay, Pagination } from "swiper/modules";
 
 import "swiper/css";
 import "swiper/css/pagination";
 
-import { Pagination } from "swiper/modules";
-import { Box } from "@mui/system";
-import TitleSingleSlide from "../components/TitleSingleSlide/TitleSingleSlide";
-
-const searchInputStyle = {
-  width: "270px",
-  margin: "40px 20px 0",
-  height: "20px",
-  backgroundColor: "209, 208, 207, .6",
+const swiperContainerStyle = {
+  maxWidth: "100%",
+  width: "100%",
+  height: "90dvh",
+  maxHeight: "100vh",
+  display: "grid",
 };
 
 function Home() {
-  const search = useSelector((state) => state.search.value);
   const apiData = useRequest(
-    search.length >= 3
-      ? `https://dolphin-app-pc6ii.ondigitalocean.app/article?q=${search}`
-      : `https://dolphin-app-pc6ii.ondigitalocean.app/article/popular`
+    `https://dolphin-app-pc6ii.ondigitalocean.app/article/popular`
   );
-
-  const dispatch = useDispatch();
-  const handleSearch = (e) => {
-    dispatch(setSearch(e.target.value));
-  };
 
   return (
     <Grid item xs={12}>
-      {/* <Grid item>
-        <TextField
-          variant="standard"
-          autoFocus
-          label="Show search"
-          id="search"
-          name="search"
-          value={search}
-          onChange={handleSearch}
-          sx={searchInputStyle}
-        />
-      </Grid> */}
-      <Box
-        className={"swiper-container"}
-        sx={{
-          maxWidth: "100%",
-          width: "100%",
-          height: "90dvh",
-          maxHeight: "100vh",
-          display: "grid",
-        }}
-      >
+      <Box className={"swiper-container"} sx={swiperContainerStyle}>
         <Swiper
           pagination={{
             dynamicBullets: true,
           }}
-          modules={[Pagination]}
+          autoplay={{
+            delay: 3500,
+            disableOnInteraction: false,
+          }}
+          modules={[Autoplay, Pagination]}
           className={"swiper-wrapper"}
-          style={{}}
         >
           {apiData.map((cardInfo, index) => (
             <SwiperSlide
